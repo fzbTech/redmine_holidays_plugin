72ba9973aeeda37232cc3d0a1cb70f36e92b5700
Peter Du <peterdu88@gmail.com>
2019-07-17 11:58:19 -0400

    Update init.rb

--- init.rb
+++ init.rb
@@ -1,5 +1,5 @@
 # Redmine Holidays Plugin
-# Copyright (C) 2014 Toshio Takiguchi
+# Copyright (C) 2019 Peter Du& Toshio Takiguchi
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,15 +16,20 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 Redmine::Plugin.register :redmine_holidays_plugin do
-  name 'Redmine Holidays Plugin'
-  author 'Toshio Takiguchi'
-  description 'This is a holiday calendar plugin for Redmine'
-  version '0.0.1'
-  url 'https://github.com/taktos/redmine_holidays_plugin'
+  name 'Dare Auto Redmine Holidays Plugin'
+  author 'Peter Du'
+  description 'This is a holiday calendar plugin for Redmine 3.4.x'
+  version '0.1.1'
+  url 'https://github.com/fzbTech/redmine_holidays_plugin'
 
   settings :default => {
-    'region' => :jp
+    'region' => :us
   }, :partial => 'settings/holidays_settings'
+end
+
+require 'holidays/core_extensions/date'
+class Date
+  include Holidays::CoreExtensions::Date
 end
 
 Holidays.load_all
